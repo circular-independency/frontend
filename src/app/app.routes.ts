11d914cc3d3127e26eabf4e0cc2357cd4f40284d
janezkoprivec <koprivec.janez@gmail.com>
2025-04-12 14:42:09 +0200

    Initialized menu and default components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { FridgeComponent } from './components/fridge/fridge.component';
+import { MenuComponent } from './components/menu/menu.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: 'fridge', component: FridgeComponent }
+];
---
+++ src/app/components/fridge/fridge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fridge',
+  imports: [],
+  templateUrl: './fridge.component.html',
+  styleUrl: './fridge.component.scss'
+})
+export class FridgeComponent {
+
+}
